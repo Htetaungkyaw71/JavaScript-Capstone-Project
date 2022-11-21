435cb9be827ee400f204fe523772b24500ee7abe
Enning94 <enningfestus@gmail.com>
2022-11-21 13:32:27 +0000

    Fix Linter Errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: ["./src/index.js",
-            './src/style.css',
-],
+  mode: 'development',
+  entry: ['./src/index.js',
+    './src/style.css',
+  ],
 
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
+      filename: '[name].css',
+    }),
   ],
 
-//   optimization: {
-//     runtimeChunk: 'single',
-//   },
+  //   optimization: {
+  //     runtimeChunk: 'single',
+  //   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [ MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
